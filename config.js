e0c05cf1f225a9ed14cb066644f84ea1916a7622
rikkqa43 <ranusuta8@gmail.com>
2025-06-02 23:13:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ function convertToBool(text, fault = "true") {
 }
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:dfAhywTzQPfaWsbQtmUtPAAPejnNIMtt@yamabiko.proxy.rlwy.net:47523",
   OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
 };
