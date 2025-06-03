8d2c2da39c1052c1dd80cfe19bcf0532a8291251
rikkqa43 <ranusuta8@gmail.com>
2025-06-03 18:02:51 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -13,7 +13,8 @@ async(robin, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sende
 try{
     await robin.sendPresenceUpdate('recording', from);
     await robin.sendMessage(from, { audio: { url: "https://github.com/rikkqa43/Bot-help/raw/refs/heads/main/synthesize.mp3" }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
-          
+    await robin.sendMessage(from,{sticker: { url : "https://github.com/rikkqa43/Bot-help/raw/refs/heads/main/STK-20231121-WA0000.webp" },package: 'S_I_H_I_L_E_L'},{ quoted: mek })   
+                 
 return await robin.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
     
 }catch(e){
