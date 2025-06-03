51989bca65eed72744ffb8ac8e4d9a74226312e3
rikkqa43 <ranusuta8@gmail.com>
2025-06-04 00:51:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "a95CQZ6Q#PnceSq7x76C6oj0TBOOMKFaDX0Cdad4p0TPPcjv6pWs",
+  SESSION_ID: process.env.SESSION_ID || "rEsTwArD#MgLanx9dZ7WcxpT5jZr6xpGqekZtGVmTWPTUfDqEsfE",
   OWNER_NUM: process.env.OWNER_NUM || "94783462955",
   PREFIX: process.env.PREFIX || ".",
   ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/nMXpJFxD/jpg.jpg",
@@ -16,5 +16,7 @@ module.exports = {
   AUTO_VOICE: process.env.AUTO_VOICE || "true",
   AUTO_STICKER: process.env.AUTO_STICKER || "true",
   AUTO_REPLY: process.env.AUTO_REPLY || "true",
+  GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyACBTtTYTuODdAiJIwVkUYUr5QlvHL_W14",
+  MOVIE_API_KEY: process.env.MOVIE_API_KEY || "sky|59fc0195c2954f8123a2b3b0063050299b4b3a62",
 };
 
